3306c6c2fe291105a9181a27fa3c055bdd76400e
Lucem Anb <lucematopz@gmail.com>
2022-12-05 16:28:38 +0300

    Basic Usage\

---
+++ client/api/README.md
@@ -0,0 +1,72 @@
+# TradingView To Anywhere
+
+### This is an API to create webhooks using [TradingView.To](https://tradingview.to) for clients that can be used to automate alerts to anywhere you want them to go.
+
+---
+
+### Supported Platforms
+- telegram
+- discord
+- google sheet
+
+---
+
+### Access
+
+The API is accessed via https endpoint. An API key and Secret is required as Authorization and User Phone Number to create, update or delete any webhooks
+1. Setup Web Template
+2. Sample Calls
+
+
+You can get your API keys by filling this form here, and the admins will get back to you
+// create a form to collect site, name, email, purpose details
+
+
+### Usage
+
+In this demo, we will use axios, a JavaScript Request library to make https calls.
+
+We created a demo setup template to offer the simplest connection. 
+This is a web page compiled using svelte that takes in your inputs using queries and generate the forms required for a successful creation, update, delete of a ussers webhook.
+
+#### Web Template
+
+https://sell.tradingview.to/setup.html?apiKey=XXXXXXXXXX&apiSecret=XXXXXXXXXX-XXX-XXXXXXXXXX&phoneNumber=1234567890
+
+Params required: 
+1. apiKey &
+2. apiSecret: you receive after approval
+3. phoneNumber: clients phone number
+4. limit: Maximum number of webhooks for a user. Default is 3
+
+
+#### API calls
+
+Endpoint: https://api.tradingview.to
+
+Authorization: Pass Headers apiKey and apiSecret
+
+Functions
+
+GET /platforms -> returns an object of allowed platforms
+
+POST /activation \
+    body requires {phoneNumber, platform, name} and platform input requirements \
+    if id is passed, an update to the id is made if the webhook is valid
+    -> returns a new webhook if successful
+
+GET /activations?phoneNumber=1234567890 \
+    optional param phoneNumber \
+    -> returns a list of webhooks
+
+POST /upgrade/id -> requests an upgrade to the webhook id
+
+DELETE /activation/id -> schedules webhook deletion in 30 days
+
+
+
+
+
+
+
+
---
+++ client/api/index.js
@@ -0,0 +1,91 @@
+const axios = require("axios")
+const API = "https://api.tradingview.to"
+
+
+const config = {
+    headers: {
+        apiKey: "hbZ27zEeHDQW3xszaIzqnb67LRJ2",
+        apiSecret: "ebe4f7f4-6b92-4bae-a3a7-1fdfc20c5f2b"
+    }
+}
+
+const sendRequest = (path, method="GET", data={}) => {
+    console.log("Sending API Request", path, method, "in 5 seconds")
+    return new Promise(resolve => {
+        setTimeout(() => {
+
+            if (method === "GET")
+                resolve(axios.get(API + path, config))
+
+            if (method === "POST")
+                resolve(axios.post(API + path, data, config))
+
+            if (method === "DELETE")
+                resolve(axios.delete(API + path, config))
+
+        }, 5000)
+    })
+}
+
+//5. Upgrade A Webhook - (IP Restriction Required)
+sendRequest( "/upgrade/8zwDEkwUOMCVGyFvAUSM", "POST").then(res => {
+
+    console.log("Webhook Expiry extended by 1 month")
+
+}).catch(error => console.error(error.message))
+
+
+return;
+
+//1. Get Platform
+sendRequest("/platforms").then(platforms => {
+
+    console.log("Platforms", platforms.data)
+
+    //2. Create Webhook - (Site Restriction Required)
+    sendRequest("/activation", "POST", {
+        phoneNumber: "254719191191",
+        platform: "telegram",
+        name: "John Doe",
+        chat: "-123456789"
+    }).then(webhook => {
+
+        console.log("Created", webhook.data)
+
+        //3. Get Webhooks - (Site Restriction Required)
+        sendRequest( "/activations?phoneNumber=254719191191").then(webhooks => {
+
+            console.log("Webhooks", webhooks.data)
+
+            //4. Update Webhook - (Site Restriction Required)
+            sendRequest( "/activation", "POST", {
+                id: webhook.data.id,
+                name: "John Doe's Telegram",
+                chat: "-123456789",
+                platform: "telegram"
+            }).then(update => {
+
+                console.log("Updated", update.data)
+
+                //5. Upgrade A Webhook - (IP Restriction Required)
+                sendRequest( "/upgrade/" + update.data.id, "POST").then(res => {
+
+                    console.log("Webhook Expiry extended by 1 month")
+
+                    //6. Delete Webhook - (Site Restriction Required)
+                    sendRequest( "/activation/" + update.data.id, "DELETE")
+                        .then(res => console.log("Scheduled For Deletion In"))
+                        .catch(error => console.error(error.message))
+
+                }).catch(error => console.error(error.message))
+
+            }).catch(error => console.error(error.message))
+
+        }).catch(error => console.error(error.message))
+
+    }).catch(error => console.error(error.message))
+
+}).catch(error => console.error(error.message))
+
+
+
